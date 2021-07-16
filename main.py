9de90ff7e2dd6cd6529dd56349348766c5f96760
SohumT <ssthadan@purdue.edu>
2021-07-16 22:54:46 +0800

    This commit establishes changes to the search functionality for ticker symbols and company names.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit
 import yfinance
+import requests
+from fuzzywuzzy import process
 import pandas
 import cufflinks
 import datetime
-
 
 streamlit.markdown('''
 
@@ -16,7 +17,24 @@ projections of stocks using LTSM
 
 streamlit.write('---')
 
-user_input = streamlit.text_input("Enter Stock Name or Ticker", " ")
+user_input = streamlit.text_input('Please Enter the Stock Ticker', ' ')
+
+
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+    result = requests.get(url).json()
+
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == symbol:
+            return x['name']
+
+
+def getCompany(text):
+    r = requests.get('https://api.iextrading.com/1.0/ref-data/symbols')
+    stock_list = r.json()
+    return process.extract(text, stock_list)
+
+
 
 
 
